e1002eb2a8924a8a034e73e312e22d2b9b3c4fe0
chris <pachopa0320@gmail.com>
2017-06-07 00:45:32 +0000

    going to the website by using longURL

---
+++ express_server.js
@@ -0,0 +1,67 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080;
+const bodyParser = require("body-parser");
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+function generateRandomString() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for(var i = 0; i < 6; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  //console.log(templateVars, "urldatabase")
+  res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  let randomId = generateRandomString();
+  //console.log(randomId, "randomID");
+  urlDatabase[randomId] = "http://" + req.body.longURL ;
+  //console.log(urlDatabase, "output is randomid : longURL");
+  //console.log(req.body, "takes the longURL"); 
+  res.redirect('/urls')     
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let shortURL = req.params.shortURL;
+  let urls = urlDatabase;
+  let longURL = urls[shortURL];
+  // console.log(req.body.id, "req.body")
+  console.log(urls[req.body.id]);
+  console.log(longURL, "longURL")
+  res.redirect(longURL);
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id, urls: urlDatabase };
+  res.render("urls_show", templateVars);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
