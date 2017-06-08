915904f5cd5fedbec3bf40b4fa01455561579c26
chris <pachopa0320@gmail.com>
2017-06-08 19:44:09 +0000

    created register page

--- express_server.js
+++ express_server.js
@@ -9,6 +9,19 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
+
 function generateRandomString() {
     var text = "";
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -36,7 +49,8 @@ app.get("/urls.json", (req, res) => {
 });
 //Homepage(list of urls)
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  let user_id = req.cookies["user_id"]
+  let templateVars = { urls: urlDatabase, user: users[user_id] };
   res.render("urls_index", templateVars);
 });
 //generate randomId
@@ -81,10 +95,33 @@ app.post("/login", (req, res) => {
   res.cookie('username', userId);
   res.redirect('/urls')     
 });
-//logout 
+//logout and clear cookie
 app.post("/logout", (req, res) => {
   let userId = req.cookies.username;
   res.clearCookie("username", userId); 
+  res.redirect('/urls')  
+});
+//register page
+app.get("/register", (req, res) => {
+  let templateVars = { user : users };
+  res.render("urls_register", templateVars);
+});
+//
+app.post("/register", (req, res) => {
+  // let templateVars = { email : req.body.email, password : req.body.password }
+  let user_id = generateRandomString();
+  let email = req.body.email;
+  let password = req.body.password;
+  if(email === '' || password === ''){
+    res.status(400).send('Email and Password cannot be empty.<br><a href="/register">return register</a>')
+  }
+  for(let userid in users) {
+    if(users[userid].email === email) {
+      res.status(400).send('Email and Password exist.<br><a href="/register">return register</a>')
+    }
+  }
+  users[user_id] = { id: user_id, email: email, password: password }
+  res.cookie('user_id', user_id)
   res.redirect('/urls')  
 });
 
